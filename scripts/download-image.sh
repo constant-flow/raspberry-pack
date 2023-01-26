e3b211e601d9835bf603ae2801ef86402fa66164
Constantin Wolf <constantin.wolf@pwc.com>
2023-01-26 10:28:07 +0100

    updated for new raspberry os compress format

--- scripts/download-image.sh
+++ scripts/download-image.sh
@@ -10,9 +10,9 @@ while [ $# -gt 0 ]; do
 done
 
 rm *.img
-curl -L $url -o temp.zip
-unzip temp.zip
-rm temp.zip
+curl -L $url -o temp.img.xz
+unxz temp.img.xz
+# rm temp.xz
 mv *.img raspberry-pack.img
 echo $url > raspberry-pack.img.version
 echo "\aImage download done."
